f598a8250fde782773fd1f46d89e9c3d52051dd4
Enrico Beyer <enrico.beyer@synaigy.com>
2023-01-22 21:00:45 +0100

    Add seeder

---
+++ prisma/seed.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+import monthsJSON from './months.json' assert { type: 'json' };
+
+const prisma = new PrismaClient();
+
+async function main() {
+	for (let i = 0; i < monthsJSON.months.length; i++) {
+		await prisma.month.upsert({
+			where: {
+				slug: monthsJSON.months[i].slug
+			},
+			update: {},
+			create: {
+				slug: monthsJSON.months[i].slug,
+				title: monthsJSON.months[i].title
+			}
+		});
+	}
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
