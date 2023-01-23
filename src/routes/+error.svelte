9e6a550b5aabdfb62836f4d92a2eb039325eaa11
Enrico Beyer <enrico.beyer@synaigy.com>
2023-01-23 22:55:20 +0100

    Add custom error boundary

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main>
+	<div class="container">
+		<h1>{$page.status}: {$page.error.message}</h1>
+		<a href="/" role="button">Zurück zur Startseite</a>
+	</div>
+</main>
