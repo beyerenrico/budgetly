80639358ebe8498c57788fd24ab5960a86c8f730
Enrico Beyer <enrico.beyer@synaigy.com>
2023-01-25 07:51:21 +0100

    Add serverSession to auth check

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,8 +1,10 @@
 import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ locals }) {
+export async function load(event) {
+	const serverSession = await getServerSession(event);
+
 	return {
-		session: locals.session
+		session: serverSession
 	};
 }
--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,14 @@
 import { redirect } from '@sveltejs/kit';
+import { getServerSession } from '@supabase/auth-helpers-sveltekit';
 import { prisma } from '$lib/server/prisma';
 
 /** @type {import('./$types').PageLoad} */
-export async function load({ locals }) {
-	if (!locals.session) {
+export async function load(event) {
+	const { locals } = event;
+
+	const serverSession = await getServerSession(event);
+
+	if (!locals.session || !serverSession) {
 		throw redirect(303, '/login');
 	}
 
