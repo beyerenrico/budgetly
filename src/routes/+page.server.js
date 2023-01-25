8b00dca1d41e23783e75719e69ae19aec3079fe2
Enrico Beyer <enrico.beyer@synaigy.com>
2023-01-25 07:25:45 +0100

    Add missing filter by user id for categories

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -4,13 +4,15 @@ import { prisma } from '$lib/server/prisma';
 /** @type {import('./$types').PageLoad} */
 export async function load({ locals }) {
 	if (!locals.session) {
-		throw redirect(303, '/register');
+		throw redirect(303, '/login');
 	}
 
 	const { user } = locals.session;
 
 	return {
-		categories: await prisma.category.findMany(),
+		categories: await prisma.category.findMany({
+			where: { userId: user.id }
+		}),
 		items: await prisma.item.findMany({
 			where: { userId: user.id },
 			include: {
